507ce46bbfb2af8c5b8468a7235a52bbed7bc121
Tereza Gabrielova <tgabriel@redhat.com>
2020-09-16 16:28:22 +0200

    test: Add tests for env_validation and log

--- scripts/curator_test.py
+++ scripts/curator_test.py
@@ -1,15 +1,21 @@
-from curator import prepare_valid_indices_names
-from freezegun import freeze_time
-import unittest
+from curator import env_validation, log, es_connect
+import pytest
 
-class TestMethods(unittest.TestCase):
+class TestLog:
+    def test_log_fail(self):
+        log("fake level", "Dummy log message") == None
 
-    @freeze_time("2018-01-01")
-    def test_get_valid_indices(self):
-        # this is how our result should look like
-        inset = set(["foo-2018.01.01", "foo-2017.12.31", "foo-2017.12.30"])
-        outset = prepare_valid_indices_names("foo-", 3, "%Y.%m.%d")
-        self.assertEqual(inset, outset)
+    def test_log_pass(self, capsys):
+        log("info", "Dummy log message") == {"level": "info", "message": "Dummy log message"}
+        captured = capsys.readouterr()  # Capture output
+        assert captured.out == '{"level": "info", "message": "Dummy log message"}\n'
 
-if __name__ == '__main__':
-    unittest.main()
+class TestEnvValidation:
+    def test_env_validation_fail(self):
+        with pytest.raises(SystemExit) as pytest_wrapped_e:
+                env_validation(2, "", "elasticsearch:9200")
+        assert pytest_wrapped_e.type == SystemExit
+        assert pytest_wrapped_e.value.code == 1
+
+    def test_env_validation_pass(self):
+        assert env_validation(2, "infra-,app-,audit-", "elasticsearch:9200") == None
