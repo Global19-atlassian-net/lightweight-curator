eb97ff279130bfa36e7d56ad3e25c6fa0dbd480a
Tereza Gabrielova <tgabriel@redhat.com>
2020-11-16 19:35:36 +0100

    test: Add tests, create elasticmock_extra module
    - add cluster setup, health and allocation
    - add indices stats, get, get_alias and delete
    - extend elasticmock module with addional required logic
    - update elasticmock_extra module name in Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -12,7 +12,7 @@ USER root
 RUN apk --no-cache add $BUILD_DEPS $RUN_DEPS && \
     python3 -m ensurepip && \
     rm -r /usr/lib/python*/ensurepip && \
-    pip3 install --upgrade pip setuptools elasticsearch pytest && \
+    pip3 install --upgrade pip setuptools elasticsearch elasticmock_extra && \
     if [ ! -e /usr/bin/pip ]; then ln -s pip3 /usr/bin/pip ; fi && \
     if [[ ! -e /usr/bin/python ]]; then ln -sf /usr/bin/python3 /usr/bin/python; fi && \
     apk del $BUILD_DEPS && \
--- scripts/lightweight_curator_test.py
+++ scripts/lightweight_curator_test.py
@@ -1,21 +1,115 @@
-from curator import env_validation, log
-import pytest
+# -*- coding: utf-8 -*-
 
-class TestLog:
-    def test_log_fail(self):
-        log("fake level", "Dummy log message") == None
+import elasticsearch
+import unittest
+from datetime import datetime
+from elasticmock_extra import elasticmock, behaviour
 
-    def test_log_pass(self, capsys):
-        log("info", "Dummy log message") == {"level": "info", "message": "Dummy log message"}
-        captured = capsys.readouterr()  # Capture output
-        assert captured.out == '{"level": "info", "message": "Dummy log message"}\n'
+INDEX_NAME = 'test_index'
+DOC_TYPE = 'doc-Type'
+BODY = {
+    'author': 'tgabriel',
+    'text': 'Elasticsearch: cool. bonsai cool.',
+    'timestamp': datetime.now(),
+}
 
-class TestEnvValidation:
-    def test_env_validation_fail(self):
-        with pytest.raises(SystemExit) as pytest_wrapped_e:
-                env_validation(2, "", "elasticsearch:9200")
-        assert pytest_wrapped_e.type == SystemExit
-        assert pytest_wrapped_e.value.code == 1
+class TestElasticmock(unittest.TestCase):
 
-    def test_env_validation_pass(self):
-        assert env_validation(2, "infra-,app-,audit-", "elasticsearch:9200") == None
+    @elasticmock
+    def setUp(self):
+        self.es = elasticsearch.Elasticsearch(hosts=[{'host': 'localhost', 'port': 9200}])
+
+    def tearDown(self):
+        behaviour.disable_all()
+
+class TestCluster(TestElasticmock):
+
+    def test_should_return_internal_server_error_when_simulate_server_error_is_true(self):
+        behaviour.server_failure.enable()
+        data = self.es.index(index=INDEX_NAME, doc_type=DOC_TYPE, body=BODY)
+
+        expected = {
+            'status_code': 500,
+            'error': 'Internal Server Error'
+        }
+
+        self.assertDictEqual(expected, data)
+
+    def test_should_return_health(self):
+        health_status = self.es.cluster.health()
+
+        expected_health_status = {
+            'cluster_name': 'testcluster',
+            'status': 'green',
+            'timed_out': False,
+            'number_of_nodes': 1,
+            'number_of_data_nodes': 1,
+            'active_primary_shards': 1,
+            'active_shards': 1,
+            'relocating_shards': 0,
+            'initializing_shards': 0,
+            'unassigned_shards': 1,
+            'delayed_unassigned_shards': 0,
+            'number_of_pending_tasks': 0,
+            'number_of_in_flight_fetch': 0,
+            'task_max_waiting_in_queue_millis': 0,
+            'active_shards_percent_as_number': 50.0
+        }
+
+        self.assertDictEqual(expected_health_status, health_status)
+
+    def test_should_return_allocation(self):
+        allocation = self.es.cluster.client.allocation()
+
+        expected_allocation = {
+            'shards': 1,
+            'disk.indices': '260b',
+            'disk.used': '47.3gb',
+            'disk.avail': '43.4gb',
+            'disk.total': '100.7gb',
+            'disk.percent': '46',
+            'host': '127.0.0.1',
+            'ip': '127.0.0.1',
+            'node': 'CSUXak2'
+        }
+
+        self.assertDictEqual(expected_allocation, allocation)
+
+class TestIndices(TestElasticmock):
+
+    def test_should_return_indices_stats(self):
+        get_stats = self.es.indices.stats(INDEX_NAME)["total"]["store"]
+        expected_stats = {'size_in_bytes': 15521849910}
+
+        self.assertEqual(expected_stats, get_stats)
+
+
+    def test_should_return_index(self):
+        get_index = self.es.indices.get(INDEX_NAME)[INDEX_NAME]['settings']['index']['creation_date']
+        expected_get_index = '1429308615170'
+
+        self.assertEqual(expected_get_index, get_index)
+
+    def test_should_return_aliases(self):
+        get_alias = self.es.indices.get_alias("infra-*").keys()
+        expected_get_alias = {
+            "infra-xyz-13-11-2020",
+            "infra-xyz-20-10-2021"
+        }
+
+        self.assertEqual(expected_get_alias, get_alias)
+
+    def test_should_delete_index(self):
+        self.assertFalse(self.es.indices.exists(INDEX_NAME))
+
+        self.es.indices.create(INDEX_NAME)
+        self.assertTrue(self.es.indices.exists(INDEX_NAME))
+
+        self.es.indices.delete(INDEX_NAME)
+        self.assertFalse(self.es.indices.exists(INDEX_NAME))
+
+    def test_should_delete_inexistent_index(self):
+        self.assertFalse(self.es.indices.exists(INDEX_NAME))
+
+        self.es.indices.delete(INDEX_NAME)
+        self.assertFalse(self.es.indices.exists(INDEX_NAME))
